4fe245e24db3ebb038ed4f748df92f88522b0275
Jianyun Tao <jianyunt@microsoft.com>
2018-07-16 15:12:12 -0700

    For nexted write-progress, we suppress the progress in the azurepsdrive.

--- AzurePSDriveWebApp.psm1
+++ AzurePSDriveWebApp.psm1
@@ -1,7 +1,10 @@
 ﻿using namespace Microsoft.PowerShell.SHiPS
 
 $script:AzureRM_WebApp = if($IsCoreCLR){'AzureRM.Websites.Netcore'}else{'AzureRM.Websites'}
-
+# Get-AzureRmWebApp cmdlet has nested write-progress. We can fix the tab completion issue in PSReadline and PSCore for not nested
+# progress. See discussion https://github.com/PowerShell/PowerShell/pull/7023 and issue https://github.com/PowerShell/PowerShell/issues/7022.
+# By suppressing the progress for Get-AzureRmWebApp below will close the line gap but it does not affect the command line ProgressPreference setting. 
+$ProgressPreference = 'SilentlyContinue'
 [SHiPSProvider(UseCache=$true)]
 class WebApps : SHiPSDirectory
 {
